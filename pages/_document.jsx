b087655e0ecb36f094dbcf8836ca9b9c6d932f34
TBSten <81161390+TBSten@users.noreply.github.com>
2021-08-25 20:29:06 +0900

    fixed

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,19 +5,21 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />
+          <title>TBSten </title>
 
-            <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-            <link rel="manifest" href="/favicons/site.webmanifest" />
-            <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-            <link rel="shortcut icon" href="/favicons/favicon.ico" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-            <meta name="theme-color" content="#ffffff" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet" />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
